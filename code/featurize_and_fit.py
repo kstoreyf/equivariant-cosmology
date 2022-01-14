50c79633155d785f8ed59b464dd6e87f133b9e40
Kate Storey-Fisher <k.sf@nyu.edu>
2022-01-14 18:18:16 -0500

    port featurization and fitting code from notebook to classes in script

---
+++ code/featurize_and_fit.py
@@ -0,0 +1,261 @@
+import numpy as np
+import sys
+import h5py
+
+# shouldn't need this if have illustris_python properly in python path! todo: check if fixed upon reload
+sys.path.insert(1, '/home/ksf293/external')
+import illustris_python as il
+
+import utils
+import scalars
+
+
+
+def run():
+    # choose parameters
+    save_tag = ''
+    #r_edges = np.logspace(np.log10(1), np.log10(1000), 4) 
+    r_edges = np.array([0, 100])
+    print(r_edges)
+    #l_arr = np.array([0, 1, 2, 3])
+    l_arr = np.array([0])
+
+    x_order_max = 0
+    m_order_max = 1
+
+    featurizer = Featurizer(r_edges, l_arr)
+    featurizer.read_simulations()
+    featurizer.match_twins()
+    featurizer.select_halos()
+    featurizer.add_info_to_halo_dicts()
+    featurizer.compute_geometric_features()
+    featurizer.compute_scalar_features(x_order_max=x_order_max, m_order_max=m_order_max)
+    featurizer.set_y_features()
+
+    fitter = Fitter(featurizer.x_scalar_features, featurizer.y_scalar, 
+                    featurizer.x_scalar_dicts)
+    fitter.split_train_test()
+    fitter.scale_and_fit()
+    fitter.predict()
+
+
+# set up paths
+class Featurizer:
+        
+    def __init__(self, r_edges, l_arr):
+        self.r_edges = r_edges
+        self.l_arr = l_arr 
+        n_rbins = len(r_edges) - 1
+        self.n_arr = np.arange(n_rbins)
+
+        self.tng_path_hydro = '/scratch/ksf293/gnn-cosmology/data/TNG50-4'
+        self.tng_path_dark = '/scratch/ksf293/gnn-cosmology/data/TNG50-4-Dark'
+        self.base_path_hydro = '/scratch/ksf293/gnn-cosmology/data/TNG50-4/output'
+        self.base_path_dark = '/scratch/ksf293/gnn-cosmology/data/TNG50-4-Dark/output'
+        self.snap_num_str = '099'
+        self.snap_num = int(self.snap_num_str)
+
+    def read_simulations(self):
+        
+        with h5py.File(f'{self.base_path_hydro}/snapdir_{self.snap_num_str}/snap_{self.snap_num_str}.0.hdf5','r') as f:
+            header = dict( f['Header'].attrs.items() )
+            self.m_dmpart = header['MassTable'][1] # this times 10^10 msun/h
+        
+        fields = ['SubhaloMass','SubhaloPos','SubhaloMassType', 'SubhaloLenType', 'SubhaloHalfmassRad', 'SubhaloGrNr']
+
+        self.subhalos_hydro = il.groupcat.loadSubhalos(self.base_path_hydro,self.snap_num,fields=fields)
+        self.halos_hydro = il.groupcat.loadHalos(self.base_path_hydro,self.snap_num)
+
+        self.subhalos_dark = il.groupcat.loadSubhalos(self.base_path_dark,self.snap_num,fields=fields)
+        self.halos_dark = il.groupcat.loadHalos(self.base_path_dark,self.snap_num)
+
+        self.idxs_halos_hydro_all = np.array(list(range(self.halos_hydro['count'])))
+        self.idxs_halos_dark_all = np.array(list(range(self.halos_dark['count'])))
+
+        self.ipart_dm = il.snapshot.partTypeNum('dm') # 0
+        self.ipart_star = il.snapshot.partTypeNum('stars') # 4
+
+
+    def match_twins(self):
+        # Load twin-matching file
+        f = h5py.File(f'{self.tng_path_hydro}/postprocessing/subhalo_matching_to_dark.hdf5','r')
+        # Note: there are two different matching algorithms: 'SubhaloIndexDark_LHaloTree' & 
+        # 'SubhaloIndexDark_SubLink'. choosing first for now
+        subhalo_full_to_dark_inds = f[f'Snapshot_{self.snap_num}']['SubhaloIndexDark_LHaloTree']
+
+        # Build dicts to match subhalos both ways. If a full subhalo has no dark subhalo twin, exclude it.
+        self.subhalo_full_to_dark_dict = {}
+        self.subhalo_dark_to_full_dict = {}
+        for i in range(len(subhalo_full_to_dark_inds)):
+            idx_full = i
+            idx_dark = subhalo_full_to_dark_inds[idx_full]
+            if idx_dark == -1:
+                continue
+            self.subhalo_dark_to_full_dict[idx_dark] = idx_full
+            self.subhalo_full_to_dark_dict[idx_full] = idx_dark
+
+    
+    def select_halos(self, num_star_particles_min=1, halo_mass_min=1e10, 
+                     halo_mass_difference_factor=3.0):
+        # GroupFirstSub: Index into the Subhalo table of the first/primary/most massive 
+        # Subfind group within this FoF group. Note: This value is signed (or should be interpreted as signed)! 
+        # In this case, a value of -1 indicates that this FoF group has no subhalos.
+        self.halos_dark['GroupFirstSub'] = self.halos_dark['GroupFirstSub'].astype('int32')
+        mask_has_subhalos = np.where(self.halos_dark['GroupFirstSub'] >= 0) # filter out halos with no subhalos
+
+        idxs_halos_dark_withsubhalos = self.idxs_halos_dark_all[mask_has_subhalos]
+        idxs_largestsubs_dark_all = self.halos_dark['GroupFirstSub'][mask_has_subhalos]
+
+        halo_dicts = []
+        for i, idx_halo_dark in enumerate(idxs_halos_dark_withsubhalos):
+            
+            idx_largestsub_dark = idxs_largestsubs_dark_all[i]
+            if idx_largestsub_dark in self.subhalo_dark_to_full_dict:
+                
+                halo_dict = {}
+                
+                # This is the index of the hydro subhalo that is the twin of the largest subhalo in the dark halo
+                idx_subtwin_hydro = self.subhalo_dark_to_full_dict[idx_largestsub_dark]
+                # This is that hydro subhalo's parent halo in the hydro sim
+                idx_halo_hydro = self.subhalos_hydro['SubhaloGrNr'][idx_subtwin_hydro]
+                # This is the largest hydro subhalo of that hydro halo
+                idx_subhalo_hydro = self.halos_hydro['GroupFirstSub'][idx_halo_hydro]
+
+                # if no stars in this subhalo, exclude
+                if self.subhalos_hydro['SubhaloLenType'][:,self.ipart_star][idx_subhalo_hydro] < num_star_particles_min: 
+                    continue
+
+                # if halo is below a minimum mass, exclude
+                halo_mass_min /= 1e10 # because Mass is in units of 10^10 M_sun/h
+                if self.halos_dark['GroupMass'][idx_halo_dark] < halo_mass_min: 
+                    continue
+                    
+                # if dark halo and hydro halo masses differ significantly, likely a mismatch; exclude
+                if self.halos_hydro['GroupMass'][idx_halo_hydro] > halo_mass_difference_factor*self.halos_dark['GroupMass'][idx_halo_dark]: 
+                    continue
+                
+                halo_dict['idx_halo_dark'] = idx_halo_dark
+                halo_dict['idx_subhalo_hydro'] = idx_subhalo_hydro
+                halo_dict['idx_subhalo_dark'] = idx_largestsub_dark
+                halo_dict['idx_halo_hydro'] = idx_halo_hydro
+                
+                halo_dicts.append(halo_dict)
+                
+        self.halo_dicts = np.array(halo_dicts)
+        self.N_halos = len(halo_dicts)
+        self.idx_halos_in_halodict = np.arange(self.N_halos)
+
+    
+    def add_info_to_halo_dicts(self):
+        for i_hd, halo_dict in enumerate(self.halo_dicts):
+            
+            idx_halo_dark = halo_dict['idx_halo_dark']
+            halo_dict['r_crit200_dark_halo'] = self.halos_dark['Group_R_Crit200'][idx_halo_dark]
+            halo_dict['mass_dark_halo_dm'] = self.halos_dark['GroupMassType'][:,self.ipart_dm][idx_halo_dark]
+
+            idx_halo_hydro = halo_dict['idx_halo_hydro']
+            halo_dict['mass_hydro_halo_dm'] = self.halos_hydro['GroupMassType'][:,self.ipart_dm][idx_halo_hydro]
+            halo_dict['mass_hydro_halo_star'] = self.halos_hydro['GroupMassType'][:,self.ipart_star][idx_halo_hydro]
+            
+            idx_subhalo_hydro = halo_dict['idx_subhalo_hydro']
+            halo_dict['mass_hydro_subhalo_star'] = self.subhalos_hydro['SubhaloMassType'][:,self.ipart_star][idx_subhalo_hydro]
+            
+    
+    def set_y_features(self, y_scalar_feature_name='mass_hydro_halo_star'):
+        self.y_scalar = np.empty(self.N_halos) # 1 mass dimension
+        for i_hd, halo_dict in enumerate(self.halo_dicts):
+            self.y_scalar[i_hd] = halo_dict[y_scalar_feature_name]
+
+    
+    def compute_geometric_features(self):
+        self.g_arrs_halos = []
+        self.g_normed_arrs_halos = []
+
+        for i_hd, halo_dict in enumerate(self.halo_dicts):
+            idx_halo_dark = halo_dict['idx_halo_dark']
+            halo_dark_dm = il.snapshot.loadHalo(self.base_path_dark,self.snap_num,idx_halo_dark,'dm')
+            x_halo_dark_dm = halo_dark_dm['Coordinates']
+            x_halo_dark_dm_com = np.mean(x_halo_dark_dm, axis=0)
+            
+            # Subtract off center of mass for each halo
+            x_data_halo = x_halo_dark_dm - x_halo_dark_dm_com
+            #print(min(x_data_halo[:,0]), max(x_data_halo[:,0]))
+            g_arrs, g_normed_arrs = scalars.get_geometric_features(x_data_halo, self.r_edges, self.l_arr, self.n_arr, self.m_dmpart)
+            self.g_arrs_halos.append(g_arrs)
+            self.g_normed_arrs_halos.append(g_normed_arrs)
+
+
+    def compute_scalar_features(self, x_order_max=0, m_order_max=1, 
+                                feature_names_to_include_also=[]):
+        
+        self.x_scalar_dicts = np.empty(self.N_halos, dtype=object)
+        self.x_scalar_features = []
+        for i_hd in range(self.N_halos):
+            scalar_dict_i = scalars.featurize_scalars(self.g_arrs_halos[i_hd], self.l_arr, self.n_arr)
+            scalars_i = []
+            for key_name, scalar_ns in scalar_dict_i.items():
+                for key_ns, scalar in scalar_ns.items():
+                    if ((scalar['x_order'] <= x_order_max and scalar['m_order'] <= m_order_max) 
+                            or key_name in feature_names_to_include_also):
+                        scalars_i.append(scalar['value'])
+            self.x_scalar_dicts[i_hd] = scalar_dict_i
+            self.x_scalar_features.append(scalars_i)
+        self.x_scalar_features = np.array(self.x_scalar_features)
+        print(self.x_scalar_features.shape)
+
+
+
+class Fitter:
+
+    def __init__(self, x_scalar_features, y_scalar, x_scalar_dicts):
+        assert x_scalar_features.shape[0]==y_scalar.shape[0], "Must have same number of x features and y labels!"
+        assert x_scalar_features.shape[0]==len(x_scalar_dicts), "Must have same number of x features and feature dicts!"
+        self.x_scalar_features = x_scalar_features
+        self.y_scalar = y_scalar
+        self.x_scalar_dicts = x_scalar_dicts
+        self.N_halos = x_scalar_features.shape[0]
+
+
+    def split_train_test(self, frac_test=0.2, seed=42):
+        # split indices and then obtain training and test x and y, so can go back and get the full info later
+        np.random.seed(seed)
+        idx_traintest = np.arange(self.N_halos)
+        idx_test = np.random.choice(idx_traintest, size=int(frac_test*self.N_halos), replace=False)
+        idx_train = np.setdiff1d(idx_traintest, idx_test, assume_unique=True)
+
+        self.x_scalar_train = self.x_scalar_features[idx_train]
+        self.x_scalar_test = self.x_scalar_features[idx_test]
+        self.y_scalar_train = self.y_scalar[idx_train]
+        self.y_scalar_test = self.y_scalar[idx_test]
+
+        self.x_scalar_dicts_train = self.x_scalar_dicts[idx_train]
+        self.x_scalar_dicts_test = self.x_scalar_dicts[idx_test]
+
+        self.n_train = len(self.x_scalar_train)
+        self.n_test = len(self.x_scalar_test)
+        self.n_parameters = self.x_scalar_features.shape[1]
+
+        print(f'n_train: {self.n_train}, n_test: {self.n_test}')
+        print(f'n_parameters: {self.n_parameters}')
+        if self.n_parameters > self.n_train/2:
+            print('WARNING!!! Number of parameters ({self.n_parameters}) is close to the number of training samples ({self.n_train})')
+
+    
+    def scale_and_fit(self):
+        self.x_scales = np.sqrt(np.mean(self.x_scalar_train**2, axis=0))
+
+        res_scalar = np.linalg.lstsq(self.x_scalar_train/self.x_scales, self.y_scalar_train, rcond=None)
+        self.theta_scalar = res_scalar[0]/self.x_scales
+
+        rank = res_scalar[2] 
+        print("rank:", rank)
+        print("n_feat:", self.x_scalar_features.shape[1])
+        assert self.theta_scalar.shape[0] == self.x_scalar_features.shape[1], 'Number of coefficients from theta vector should equal number of features!'
+
+    
+    def predict(self):
+        self.y_scalar_pred = self.x_scalar_test @ self.theta_scalar
+
+            
+if __name__=='__main__':
+    run()
