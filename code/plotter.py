d6ce53ed05b3f3704a2f913bd06e58d11f6d7f0d
Kate Storey-Fisher <k.sf@nyu.edu>
2022-03-24 11:29:29 -0400

    updates to plotting code

--- code/plotter.py
+++ code/plotter.py
@@ -137,7 +137,7 @@ def plot_halos_dark_and_hydro(halo_dicts, base_path_dark, base_path_hydro, snap_
 
 
 def plot_pred_vs_true(y_true, y_pred, y_train, y_train_pred, 
-                      fitter, msfe_test, chi2_train, mass_multiplier,
+                      text_results='', 
                       title=None, save_fn=None, y_lim=None):
     fig = plt.figure(figsize=(6,6))
     ax = plt.gca()
@@ -170,11 +170,7 @@ def plot_pred_vs_true(y_true, y_pred, y_train, y_train_pred,
         plt.xlim(0.5*m_minmin, 2*m_maxmax)
         plt.ylim(0.5*m_minmin, 2*m_maxmax)
 
-    n_neg = len(np.where(fitter.y_scalar_pred*mass_multiplier < 0)[0])
-    plt.text(0.1, 0.9, fr'$n_\mathrm{{features}}$: {fitter.n_A_features}, rank: {fitter.rank}' '\n'
-                       fr'MSFE: {msfe_test:.3f}, $n_\mathrm{{test}}$: {fitter.n_test}' '\n'
-                       fr'$\chi^2$: {chi2_train:.3e}, $n_\mathrm{{train}}$: {fitter.n_train}' '\n'
-                       fr'# m_pred < 0: {n_neg}', 
+    plt.text(0.1, 0.9, text_results, 
              transform=ax.transAxes, verticalalignment='top', fontsize=12)
     plt.title(title)
     plt.legend(loc='lower right', fontsize=12)
@@ -185,7 +181,7 @@ def plot_pred_vs_true(y_true, y_pred, y_train, y_train_pred,
 
 
 def plot_pred_vs_mass(mass, y_true, y_pred, mass_train, y_train, y_train_pred, 
-                      fitter, msfe_test, chi2_train, mass_multiplier,
+                      text_results='',
                       title=None, save_fn=None, overplot_function=None,
                       logx='log', logy='log', y_lim=None):
     fig = plt.figure(figsize=(8,6))
@@ -223,15 +219,10 @@ def plot_pred_vs_mass(mass, y_true, y_pred, mass_train, y_train, y_train_pred,
     #plt.xlim(0.5*mass_minmin, 2*mass_maxmax)
     #plt.ylim(0.5*y_minmin, 2*y_maxmax)
 
-    n_neg = len(np.where(fitter.y_scalar_pred*mass_multiplier < 0)[0])
-    plt.text(0.1, 0.9, fr'$n_\mathrm{{features}}$: {fitter.n_A_features}, rank: {fitter.rank}' '\n'
-                       fr'MSFE: {msfe_test:.3f}, $n_\mathrm{{test}}$: {fitter.n_test}' '\n'
-                       fr'$\chi^2$: {chi2_train:.3e}, $n_\mathrm{{train}}$: {fitter.n_train}' '\n'
-                       fr'# m_pred < 0: {n_neg}', 
+    plt.text(0.1, 0.9, text_results, 
              transform=ax.transAxes, verticalalignment='top', fontsize=12)
     plt.title(title)
     plt.legend(loc='lower right', fontsize=12)
-
     
     # save
     if save_fn is not None:
