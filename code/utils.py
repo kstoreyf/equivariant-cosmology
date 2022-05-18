6f8b5620ca9681f9b8b2ab9bcb71aafc434bd0d2
Kate Storey-Fisher <k.sf@nyu.edu>
2022-05-18 19:38:18 -0400

    remove old code

--- code/utils.py
+++ code/utils.py
@@ -4,23 +4,6 @@ from scipy.optimize import curve_fit
 from geometric_features import GeometricFeature
 
 
-def unroll_array(arr, n_dim=3):
-    # shape (N, n_feat, n_dim) to (N*n_dim, n_feat)
-    # ... indexes all previous dimensions with :
-    arr_unrolled = np.vstack([arr[...,d] for d in range(n_dim)])
-    return arr_unrolled
-
-def unroll_array_2d(arr, n_dim=3):
-    # shape (N, n_dim) to (N*n_dim)
-    arr_unrolled = np.hstack([arr[:,d] for d in range(n_dim)])
-    return arr_unrolled
-
-def roll_array(arr, n_dim=3):
-    # shape (N*n_dim) to (N, n_dim)
-    assert arr.shape[0] % n_dim == 0, "Unrolled vector shape must be a multiple of n_dim!"
-    N = int( arr.shape[0] / n_dim )
-    arr_rolled = np.array([arr[d*N:(d+1)*N] for d in range(n_dim)]).T
-    return arr_rolled
 
 def get_alt_sim_name(sim_name):
     sim_name_dict = {'TNG100-1': 'L75n1820TNG',
