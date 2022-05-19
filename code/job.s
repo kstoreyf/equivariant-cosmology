119f416a9506af27be818ae247d95c5bc82689c2
Kate Storey-Fisher <k.sf@nyu.edu>
2022-05-18 22:31:10 -0400

    update job script

--- code/job.s
+++ code/job.s
@@ -1,7 +1,7 @@
 #!/bin/bash
-##SBATCH --job-name=halo_selector_tng100_nstarmin1_twin
-##SBATCH --job-name=geo_featurizer_tng100_nstarmin1_twin_xminPE_rall
-#SBATCH --job-name=scalar_featurizer_tng100_twin_rall_mord2_xord4_vord4
+##SBATCH --job-name=halo_selector_tng50_nstarmin1_twin
+#SBATCH --job-name=geo_featurizer_tng100_nstarmin1_twin_xminPE_rall
+##SBATCH --job-name=scalar_featurizer_tng50_twin_rall_mord2_xord4_vord4
 #SBATCH --output=logs/%x.out
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
@@ -17,8 +17,8 @@ exec --overlay $overlay_ext3:ro \
 /bin/bash; \
 cd /home/ksf293/equivariant-cosmology/code; \
 #python run_halo_selector.py;
-#python run_geometric_featurizer.py;
-python run_scalar_featurizer.py;
+python run_geometric_featurizer.py;
+#python run_scalar_featurizer.py;
 "
 
 
