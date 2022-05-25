7f0bab0d0c02748fea64ff3ff8d7962cd9d0c4e5
Kate Storey-Fisher <k.sf@nyu.edu>
2022-05-25 11:00:06 -0400

    run halo selection with higher min particles

--- code/job.s
+++ code/job.s
@@ -1,12 +1,12 @@
 #!/bin/bash
-##SBATCH --job-name=halo_selector_tng50_nstarmin1_twin
-##SBATCH --job-name=geo_featurizer_tng100_nstarmin1_twin_xminPE_rall
+#SBATCH --job-name=halo_selector_tng100_nstarmin10_twin_and_geo_feat
+##SBATCH --job-name=geo_featurizer_tng100_nstarmin1_twin_xminPEsub_rall
 ##SBATCH --job-name=scalar_featurizer_tng100_twin_rall_mord2_xord2_vord2
-#SBATCH --job-name=feature_importance_tng100_top30
+##SBATCH --job-name=feature_importance_tng100_top30
 #SBATCH --output=logs/%x.out
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
-#SBATCH --mem=10GB
+#SBATCH --mem=60GB
 #SBATCH --time=24:00:00
 
 cd ~
@@ -17,10 +17,10 @@ exec --overlay $overlay_ext3:ro \
 -c "source /ext3/env.sh; \
 /bin/bash; \
 cd /home/ksf293/equivariant-cosmology/code; \
-#python run_halo_selector.py;
-#python run_geometric_featurizer.py;
+python run_halo_selector.py;
+python run_geometric_featurizer.py;
 #python run_scalar_featurizer.py;
-python feature_importance.py;
+#python feature_importance.py;
 "
 
 
